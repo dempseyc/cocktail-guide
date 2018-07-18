2e40b80c3954b1f4f1390f85fa5fe351807ddc81
Craig Dempsey <craigdempsey@gmail.com>
2018-07-18 12:47:56 -0400

    remove unused files

--- client/src/actions/invalidateSubreddit.js
+++
@@ -1,8 +0,0 @@
-export const INVALIDATE_SUBREDDIT = 'INVALIDATE_SUBREDDIT'
-​
-export function invalidateSubreddit(subreddit) {
-  return {
-    type: INVALIDATE_SUBREDDIT,
-    subreddit
-  }
-}
--- client/src/actions/selectSubreddit.js
+++
@@ -1,8 +0,0 @@
-export const SELECT_SUBREDDIT = 'SELECT_SUBREDDIT'
-​
-export function selectSubreddit(subreddit) {
-  return {
-    type: SELECT_SUBREDDIT,
-    subreddit
-  }
-}
